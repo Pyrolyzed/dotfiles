77fe7763ea97ffea8484ac5b3f2cac636263c3b0
pyro <pyrolyzed@proton.me>
2025-12-03 21:55:38 -0600

    Removed unnecessary imports

--- modules/base.nix
+++ modules/base.nix
@@ -7,7 +7,6 @@ in
     {
       config,
       pkgs,
-      lib,
       ...
     }:
     {
--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,8 +1,6 @@
 {
   flake.modules.nixos.network =
     {
-      config,
-      pkgs,
       lib,
       ...
     }:
